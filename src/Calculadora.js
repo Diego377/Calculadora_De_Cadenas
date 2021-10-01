62a70979fc6dfcf268883ebeada3c96a4de79270
DiegoDelgadillo <ddcabrera69@gmail.com>
2021-10-01 19:16:18 -0400

    suma de dos numeros separados por guion

--- src/Calculadora.js
+++ src/Calculadora.js
@@ -1,6 +1,15 @@
 export function Calculadora(cadena) {
-    let cadenaDividida = cadena.split(",")
+    let separadores = [",","-"]
+    let aux = 0
     let suma = 0
+    let auxCar = ","
+    separadores.forEach(function(caracter){
+        aux = cadena.search(caracter);
+        if(aux != -1){
+            auxCar = caracter
+        }
+    });
+    let cadenaDividida = cadena.split(auxCar)
     for(let i in cadenaDividida){
         suma = suma + parseInt(cadenaDividida[i],10)
     }
--- src/Calculadora.test.js
+++ src/Calculadora.test.js
@@ -1,11 +1,12 @@
 import {Calculadora} from "./Calculadora";
 describe("Calculadora ", () => {
-    it("deberia retornar la suma de dos numeros", () => {
+    it("deberia retornar la suma de dos numeros separados por coma", () => {
       expect(Calculadora("2,3")).toEqual(5);
     });
-    it("deberia retornar la suma de los numeros en la cadena", () => {
+    it("deberia retornar la suma de los numeros en la cadena separados por coma", () => {
         expect(Calculadora("1,2,4,8")).toEqual(15);
       });
+      it("deberia retornar la suma de dos numeros separados por guion", () => {
+        expect(Calculadora("2-3")).toEqual(5);
+      });
   });
-
-  
