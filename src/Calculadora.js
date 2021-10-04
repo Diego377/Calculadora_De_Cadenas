3271653e584a6029b2b7e52715918af4125300c6
DiegoDelgadillo <ddcabrera69@gmail.com>
2021-10-04 18:08:05 -0400

    numeros menores a 1000

--- src/Calculadora.js
+++ src/Calculadora.js
@@ -4,30 +4,25 @@ export function Calculadora(cadena) {
     let suma = 0
     let auxCar = ""
     let i = 0
+    let cadenaDividida
 
     separadores.forEach(function(caracter){
         aux = cadena.search(caracter);
         if(aux != -1){
             auxCar = caracter
-            let cadenaDividida = dividirCadena(cadena, auxCar)
+            cadenaDividida = dividirCadena(cadena, auxCar)
             for(i in cadenaDividida){
-                if(parseInt(cadenaDividida[i],10) < 1000)
+                if(parseInt(cadenaDividida[i],10) < 1000){
                     suma = suma + parseInt(cadenaDividida[i],10)
+                }
             }
         }
     });
+    
 
     return suma;
 }
 function dividirCadena(cadena, caracter){
     let cadenaDividida = cadena.split(caracter)
     return cadenaDividida
-}
-
-function verificarNumero(cadenaDividida){
-    let suma = 0
-    if(isNaN(parseInt(cadenaDividida[i],10)) == false){
-        suma = suma + parseInt(cadenaDividida[i],10)
-    }
-    return suma
 }
